2ce5b468bce099d25e598ef425ea42b2ca6466bc
Reinaldo Mizutani <30627739+reinaldomizutani@users.noreply.github.com>
2017-10-05 14:12:02 -0300

    Add files via upload

--- estatico.c
+++ estatico.c
@@ -1,4 +1,53 @@
 #include "estatico.h"
+#include "pilha.h"
+#include "fila.h"
+
+void imprime(){
+    int i = 0;
+    scanf("%d", &i);
+
+        system("clear");
+    printf("=====================================================================\n");
+    printf("=                     Sistema de Biblioteca                         =\n");
+    printf("=                                                                   =\n");
+    printf("=      Escolha a opcao desejada:                                    =\n");
+    printf("=      1 - Cadastrar novo usuário                                   =\n");
+    printf("=      2 - cadastrar novo livro no acervo                           =\n");
+    printf("=      3 - Retirar livro                                            =\n");
+    printf("=      4 - Devolver livro                                           =\n");
+    printf("=      5 - Remover livro do acervo                                  =\n");
+    printf("=      6 - Remover usuário                                          =\n");
+    printf("=                                                                   =\n");
+    printf("=                                                                   =\n");
+    printf("=      digite sua opcao:                                            =\n");
+    printf("=                                                                   =\n");
+    printf("=                                                                   =\n");
+    printf("=                                                                   =\n");
+    printf("=====================================================================\n");
+
+
+    while(i!=-1){
+    system("clear");
+    printf("=====================================================================\n");
+    printf("=                     Sistema de Biblioteca                         =\n");
+    printf("=                                                                   =\n");
+    printf("=      Escolha a opcao desejada:                                    =\n");
+    printf("=      1 - Cadastrar novo usuário                                   =\n");
+    printf("=      2 - cadastrar novo livro no acervo                           =\n");
+    printf("=      3 - Retirar livro                                            =\n");
+    printf("=      4 - Devolver livro                                           =\n");
+    printf("=      5 - Remover livro do acervo                                  =\n");
+    printf("=      6 - Remover usuário                                          =\n");
+    printf("=                                                                   =\n");
+    printf("=                                                                   =\n");
+    printf("=      digite sua opcao:                                            =\n");
+    printf("=                                                                   =\n");
+    printf("=                                                                   =\n");
+    printf("=                                                                   =\n");
+    printf("=====================================================================\n");
+    scanf("%d", &i);
+    }
+}
 
 void cria(banco *B) {
      int i;
@@ -8,54 +57,66 @@ void cria(banco *B) {
      B->pv=0;
      B->ini=-1;
      B->fim=-1;
-     
 }
-void getnode(banco *B, int *pos) { //retorna para *pos o primeiro vazio e troca o ï¿½ndice da lista de vazios para a prï¿½xima posiï¿½ï¿½o
-    if (B->pv != -1) {
-       *pos = B->pv;
-       B->pv = B->itens[B->pv].prox;
-       B->itens[*pos].prox = -1;
+
+void getnode(banco *B, int *pos) {
+    if (B->pv!=-1) {
+       *pos=B->pv;
+       B->pv=B->itens[B->pv].prox;
+       B->itens[*pos].prox=-1;
     }
     else *pos=-1;
 }
-void freenode(banco *B, int *pos) { //insere  a posiï¿½ï¿½o *pos no primeiro vazio e aponta o prï¿½ximo para o primeiro vazio
-    B->itens[*pos].prox = B->pv;
-    B->pv = *pos;
+void freenode(banco *B, int *pos) {
+    B->itens[*pos].prox=B->pv;
+    B->pv=*pos;
 }
-int EstaVazio(banco *B) { //retorna 1 se o banco estiver vazio
+int EstaVazio(banco *B) {
     if (B->ini==-1)
        return(1);
     else return(0);
 }
-int EstaCheio(banco *B) { // retorna 1 se o banco estiver cheio.
+int EstaCheio(banco *B) {
     if (B->pv==-1)
        return(1);
     else return(0);
 }
-void cadastraLivro(banco *B, livro *x, int *erro) {
+/*void inserirAluno_Comeco(banco *B, aluno *X, int *erro) {
      int pos;
      if (EstaCheio(B))
         *erro=1;
      else {
           *erro=0;
           getnode(B,&pos);
-          B->itens[pos].infoLivro=*x;
-          B->itens[pos].prox=-1;
-          if (B->fim!=-1)
-              B->itens[B->fim].prox=pos;
-          B->fim=pos;
+          B->itens[pos].usuarios=*X;
+          B->itens[pos].prox=B->ini;
+          B->ini=pos;
+          if (B->fim==-1)
+             B->fim=pos;
+     }
+}*/
+void retirarAluno(banco *B, aluno *x, int *erro) {
+     int pos;
+     if (EstaVazio(B))
+        *erro=1;
+     else {
+          *erro=0;
+          *x=B->itens[B->ini].usuarios;
+          pos=B->ini;
+          B->ini=B->itens[B->ini].prox;
           if (B->ini==-1)
-              B->ini=pos;
+             B->fim=-1;
+          freenode(B,&pos);
      }
 }
-void cadastraAluno(banco *B, aluno *x, int *erro) {
+void inserirAluno_fim(banco *B, aluno *x, int *erro) {
      int pos;
      if (EstaCheio(B))
         *erro=1;
      else {
           *erro=0;
           getnode(B,&pos);
-          B->itens[pos].infoAluno=*x;
+          B->itens[pos].usuarios=*x;
           B->itens[pos].prox=-1;
           if (B->fim!=-1)
              B->itens[B->fim].prox=pos;
@@ -65,75 +126,144 @@ void cadastraAluno(banco *B, aluno *x, int *erro) {
      }
 }
 
-void insereFilaEspera(banco *B, aluno *X){
-    int pos;
-    if(estacheio(B)){
-        *erro = 1;
+void retirarLivro_comeco(banco *B, livro *x, int *erro) {
+     int pos;
+     if (EstaVazio(B))
+        *erro=1;
+     else {
+          *erro=0;
+          *x=B->itens[B->ini].acervo;
+          pos=B->ini;
+          B->ini=B->itens[B->ini].prox;
+          if (B->ini==-1)
+             B->fim=-1;
+          freenode(B,&pos);
+     }
+}
+void inserirLivro_fim(banco *B, livro *x, int *erro) {
+     int pos;
+     if (EstaCheio(B))
+        *erro=1;
+     else {
+          *erro=0;
+          getnode(B,&pos);
+          B->itens[pos].acervo=*x;
+          B->itens[pos].prox=-1;
+          if (B->fim!=-1)
+             B->itens[B->fim].prox=pos;
+          B->fim=pos;
+          if (B->ini==-1)
+             B->ini=pos;
+     }
+}
+
+void retirarLivro (banco *B, livro *x, int *erro){
+    if (!EstaVazio(B)){
+        *erro=0;
+            if (B->itens->acervo->qtd==-1){
+                EntraFila(F, X, erro);
+            }
+            else{
+                retirarLivro_comeco(B, x, erro);
+            }
     }
     else{
-        *erro = 0;
-        getnode(B, &pos);
-        
+        *erro=1;
     }
-}
-
-
-
-
-
-
-
-
-void retirarAluno(banco *B, aluno *x, int *erro) {
-     int pos;
-     if (EstaVazio(B))
-        *erro=1;
-     else {
-          *erro=0;
-          *x=B->itens[B->ini].infoAluno;
-          pos=B->ini;
-          B->ini=B->itens[B->ini].prox;
-          if (B->ini==-1)
-             B->fim=-1;
-          freenode(B,&pos);
-     }
-}
-
-
-void retirarLivro(banco *B, livro *x, int *erro) {
-     int pos;
-     if (EstaVazio(B))
-        *erro=1;
-     else {
-          *erro=0;
-          *x=B->itens[B->ini].infoLivro;
-          pos=B->ini;
-          B->ini=B->itens[B->ini].prox;
-          if (B->ini==-1)
-             B->fim=-1;
-          freenode(B,&pos);
-     }
-}
-
-
-void imprimeLivros (banco *B){
-int i = B->ini;
-livro liv;
-system("cls");
-while(i != -1){
-    liv = B->itens[i].infoLivro;
-    printf("\n\n%s\n%s\n%s\n\n", liv.titulo, liv.autor, liv.editora);
-    i = B->itens[i].prox;
-}
 }
 
 void imprimeAlunos (banco *B){
-int i = B->ini;
-aluno al;
-system("cls");
-while(i != -1){
-    al = B->itens[i].infoAluno;
-    printf("\n\nnome: %s\nnusp: %d\nemail: %s\ntelefone: %s\n", al.nome, al.nusp, al.email, al.telefone);
-    i = B->itens[i].prox;
+    int i = B->ini;
+    aluno al;
+
+    while(i != -1){
+        al = B->itens[i].usuarios;
+        printf("\n\n%s\n%s\n%s\n%s\n\n", al.nome, al.nusp, al.email, al.telefone);
+        i = B->itens[i].prox;
+    }
 }
+
+
+//FUNCOES DA PILHA
+void Create(Pilha *p){
+	p->topo = -1;
+	p->total = 0;
+	return;
+}
+
+void Empty(Pilha *p){
+	p->topo = -1;
+	return;
+}
+
+int IsEmpty(Pilha *P){
+	if(P->topo == -1)
+		return 1;
+	else return 0;
+}
+
+int IsFull(Pilha *P){
+	if(P->topo == TamP-1)
+		return 1;
+	else return 0;
+}
+
+int Push(Pilha *P, elem *X){
+
+	if(!IsFull(P)){
+		P->topo++;
+		P->total++;
+		P->itens[P->topo] = *X;
+		return 0;
+	} else return 1;
+
+//FUNÇÕES DA FILA
+void CriaFila(Fila *F) {
+     F->inicio=0;
+     F->fim=0;
+     F->total=0;
+     return;
+}
+
+void EsvaziaFila(Fila *F) {
+     F->inicio=0;
+     F->fim=0;
+     F->total=0;
+     return;
+}
+
+int EstaVaziaFila(Fila *F) {
+    if (F->total==0)
+       return 1;
+    else return 0;
+}
+
+int EstaCheiaFila(Fila *F) {
+    if (F->total==TamF)
+       return 1;
+    else return 0;
+}
+
+void EntraFila(Fila *F, elem *X, int *erro) {
+     if (!EstaCheiaFila(F)) {
+          *erro=0;
+          (F->total)++;
+          F->itens[F->fim]=*X;
+          if (F->fim==TamF-1) F->fim=0;
+          else F->fim++;
+     }
+     else *erro=1;
+     return;
+}
+
+void SaiFila(Fila *F, elem *X, int *erro) {
+     if (!EstaVaziaFila(F)) {
+          *erro=0;
+          F->total--;
+          *X=F->itens[F->inicio];
+          if (F->inicio==TamF-1) F->inicio=0;
+          else F->inicio++;
+     }
+     else *erro=1;
+     return;
 }
